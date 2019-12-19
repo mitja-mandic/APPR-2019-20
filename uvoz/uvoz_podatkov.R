db93fd05b6dea7d0223501f938f4f1ecc7a851dc
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2019-12-19 17:30:10 +0100

    Dodane tabele, preurejena glavna tabela

--- uvoz/uvoz_podatkov.R
+++ uvoz/uvoz_podatkov.R
@@ -1,3 +1,12 @@
 library(readr)
 library(dplyr)
+library(rvest)
+library(XML)
+library(gsubfn)
+library(tidyr)
+
+
 StarostneStrukture_deležPrebivalstva <- read_csv("podatki/starostneStrukture_procentPopulacije.csv", na = ("..")) %>% select(-'Series Code', -'Country Code')
+
+url <- "https://en.wikipedia.org/wiki/List_of_countries_by_past_and_projected_GDP_(PPP)"
+stran <- read_html(url)
