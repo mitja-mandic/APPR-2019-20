0ab5f9d2b52ba017c061c5bb0e320c24c23128c0
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2019-12-20 20:04:27 +0100

    Začetek dela s podatki s spleta

--- uvoz/uvoz_podatkov.R
+++ uvoz/uvoz_podatkov.R
@@ -4,9 +4,32 @@ library(rvest)
 library(XML)
 library(gsubfn)
 library(tidyr)
+library(stringr)
+StarostneStrukture_delezPrebivalstva <- read_csv("podatki/podatki.csv", na = ("..")) %>% select(-'Series Code', -'Country Code')
+colnames(StarostneStrukture_delezPrebivalstva) <- c("Age_group", "country", leta)
+leta <- seq(1960, 2015, by=5)
+
+starost0.14 <- StarostneStrukture_delezPrebivalstva %>% filter(Age_group == "Population ages 0-14 (% of total population)") %>%
+  select(-Age_group) %>%
+  gather(year, percentage, as.character(leta)) %>%
+  arrange(country)
 
 
-StarostneStrukture_deležPrebivalstva <- read_csv("podatki/starostneStrukture_procentPopulacije.csv", na = ("..")) %>% select(-'Series Code', -'Country Code')
+starost15.65 <- StarostneStrukture_delezPrebivalstva %>% filter(Age_group == "Population ages 15-64 (% of total population)") %>%
+  select(-Age_group) %>%
+  gather(year, percentage, as.character(leta)) %>%
+  arrange(country)
+
+starost65 <- StarostneStrukture_delezPrebivalstva %>% filter(Age_group == "Population ages 65 and above (% of total population)") %>%
+  select(-Age_group) %>%
+  gather(year, percentage, as.character(leta)) %>%
+  arrange(country)
+
 
 url <- "https://en.wikipedia.org/wiki/List_of_countries_by_past_and_projected_GDP_(PPP)"
 stran <- read_html(url)
+
+tabela_wiki <- stran %>% 
+  html_nodes(xpath="//table[@class='sortable wikitable']//td") %>%
+  html_text() %>% str_replace_all(',','')
+
