3c1f95701895eb32046eace978833c66c7d6073c
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2019-12-23 19:50:25 +0100

    Popravljen uvoz tabel iz wikipedie.

--- uvoz/uvoz_podatkov.R
+++ uvoz/uvoz_podatkov.R
@@ -29,7 +29,36 @@ starost65 <- StarostneStrukture_delezPrebivalstva %>% filter(Age_group == "Popul
 url <- "https://en.wikipedia.org/wiki/List_of_countries_by_past_and_projected_GDP_(PPP)"
 stran <- read_html(url)
 
-tabela_wiki <- stran %>% 
-  html_nodes(xpath="//table[@class='sortable wikitable']//td") %>%
-  html_text() %>% str_replace_all(',','')
+
+gpd.ppp.8089 <- stran %>% html_node(xpath = "//*[@id = 'mw-content-text']/div/table") %>%
+  html_table(fill=TRUE)
+
+colnames(gpd.ppp.8089) <- c("country", leta = 1980:1989)
+
+
+gpd.ppp.8089 <- gpd.ppp.8089 %>% gather(year, bdp, as.character(1980:1989)) %>% 
+  arrange(country)
+
+gdp.ppp.9099 <- stran %>% html_node(xpath = "//*[@id = 'mw-content-text']/div/table[2]") %>%
+  html_table(fill=TRUE)
+colnames(gdp.ppp.9099) <- c("country", 1990:1999)
+
+
+
+gpd.ppp.9099 <- gpd.ppp.9099 %>% gather(year, bdp, as.character(1990:1999)) %>% 
+  arrange(country)
+
+gdp.ppp.0009 <- stran %>% html_node(xpath = "//*[@id = 'mw-content-text']/div/table[3]") %>%
+  html_table(fill=TRUE)
+colnames(gdp.ppp.0009) <- c("country", 2000:2009)
+gpd.ppp.0009<- gpd.ppp.0009 %>% gather(year, bdp, as.character(2000:2009)) %>% 
+  arrange(country)
+
+gpd.ppp.1019 <- stran %>% html_node(xpath = "//*[@id = 'mw-content-text']/div/table[4]") %>%
+  html_table(fill=TRUE)
+colnames(gdp.ppp.1019) <- c("country", 2010:2019)
+gpd.ppp.1019 <- gpd.ppp.1019 %>% gather(year, bdp, as.character(2010:2019)) %>% 
+  arrange(country)
+
+
 
