b9f3aa4361053f07d44a77801c4b84322cf7f605
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2020-01-09 21:36:47 +0100

    Upoštevan issue, še dobro da sem ga tako kmalu opazil...

--- uvoz/uvoz_podatkov.R
+++ uvoz/uvoz_podatkov.R
@@ -5,9 +5,9 @@ library(XML)
 library(gsubfn)
 library(tidyr)
 library(stringr)
+leta <- seq(1960, 2015, by=5)
 StarostneStrukture_delezPrebivalstva <- read_csv("podatki/podatki.csv", na = ("..")) %>% select(-'Series Code', -'Country Code')
 colnames(StarostneStrukture_delezPrebivalstva) <- c("Age_group", "country", leta)
-leta <- seq(1960, 2015, by=5)
 
 starost0.14 <- StarostneStrukture_delezPrebivalstva %>% filter(Age_group == "Population ages 0-14 (% of total population)") %>%
   select(-Age_group) %>%
@@ -33,31 +33,41 @@ stran <- read_html(url)
 gdp.ppp.8089 <- stran %>% html_node(xpath = "//*[@id = 'mw-content-text']/div/table[2]") %>%
   html_table(fill=TRUE)
 colnames(gdp.ppp.8089) <- c("country", 1980:1989)
-osemdeseta = seq(1980, 1989, by=5)
+osemdeseta <- seq(1980, 1989, by=5)
 gdp.ppp.8089 <- gdp.ppp.8089 %>% select(c('country',"1980","1985"))
 
-gpd.ppp.8089 <- gpd.ppp.8089 %>% gather(year, bdp, as.character(osemdeseta))   %>%
+gdp.ppp.8089 <- gdp.ppp.8089 %>% gather(year, bdp, as.character(osemdeseta)) %>%
   arrange(country)
 
 #devetdeseta
 gdp.ppp.9099 <- stran %>% html_node(xpath = "//*[@id = 'mw-content-text']/div/table[3]") %>%
   html_table(fill=TRUE)
-colnames(gdp.ppp.9099) <- c("country", 1990:1999)
-gpd.ppp.9099<- gpd.ppp.9099 %>% gather(year, bdp, as.character(1990:1999)) %>% 
-  arrange(country)
 
+colnames(gdp.ppp.9099) <- c("country", 1990:1999)
+
+gdp.ppp.9099 <- gdp.ppp.9099 %>% select(c("country", "1990","1995"))
+
+devetdeseta <- seq(1990, 1999, by=5)
+
+gdp.ppp.9099 <- gdp.ppp.9099 %>% gather(year, bdp, as.character(devetdeseta)) %>% 
+  arrange(country)
 
 #2000-2010
 gpd.ppp.0009 <- stran %>% html_node(xpath = "//*[@id = 'mw-content-text']/div/table[4]") %>%
   html_table(fill=TRUE)
-colnames(gdp.ppp.0009) <- c("country", 2000:2009)
-gpd.ppp.0009 <- gpd.ppp.0009 %>% gather(year, bdp, as.character(2000:2009)) %>% 
+
+colnames(gpd.ppp.0009) <- c("country", 2000:2009)
+
+#gdp.ppp.0009 <- gpd.ppp.0009 %>% select(c("country", "2000", "2005"))
+
+gpd.ppp.0009 <- gpd.ppp.0009 %>% gather(year, bdp, c("2000", "2005")) %>% 
   arrange(country)
 
 
 #2010 - 2019
-gpd.ppp.1019 <- stran %>% html_node(xpath = "//*[@id = 'mw-content-text']/div/table[5]") %>%
-  html_table(fill=TRUE)
-colnames(gdp.ppp.1019) <- c("country", 2010:2019)
-gpd.ppp.1019 <- gpd.ppp.1019 %>% gather(year, bdp, as.character(2010:2019)) %>% 
-  arrange(country)
+#gpd.ppp.1019 <- stran %>% html_node(xpath = "//*[@id = 'mw-content-text']/div/table[5]") %>%
+#  html_table(fill=TRUE)
+#colnames(gdp.ppp.1019) <- c("country", 2010:2019)
+#gdp.ppp.1019 <- gdp.ppp.1019 %>% select(c("country", "2010", "2015"))
+#gpd.ppp.1019 <- gpd.ppp.1019 %>% gather(year, bdp, c("2010","2015")) %>% 
+  #arrange(country)
