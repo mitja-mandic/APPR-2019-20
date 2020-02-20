549a8c1314f48b134df7edcf4728b404300f989a
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2020-02-20 09:37:15 +0100

    Igranje z grafi, dodani grafi procent bdp za posamezna leta

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,15 +1,30 @@
+
+starost64 <- StarostneStrukture_janos %>% filter(Age_group == "65 in vec") %>% filter(year == "1960")
+
+
+bdp_starostneStrukture65 <- inner_join(bdpji, StarostneStrukture_janos, by = c("year", "country")) %>% 
+  filter(Age_group == "65 in vec")
+
+bdp_starostne65_graf <- ggplot(bdp_starostneStrukture65, aes(x=gdp, y=percentage)) +
+  geom_point() + scale_x_log10() + facet_wrap(~year)
+
+# + geom_hline(yintercept = group_by(bdp_starostneStrukture65, year) %>%
+#               summarise(povp = mean(percentage)))
+
+print(bdp_starostne65_graf)
+
+
+
+
 starost15_64 <- StarostneStrukture_janos %>% filter(Age_group == "15-64") %>% filter(year == "2015")
 starost15_64_graf <- ggplot(starost15_64, aes(x = percentage)) +
   geom_histogram()
 print(starost15_64_graf)
 
-starost64 <- StarostneStrukture_janos %>% filter(Age_group == "65 in vec") %>% filter(year == "1960")
+
 
 zemljevid64 <- tm_shape(merge(svet, starost64, by.x = "NAME", by.y = "country")) + 
   tm_polygons("percentage", midpoint = 0.2)
-print(zemljevid64)
 
 zemljevid15_64 <- tm_shape(merge(svet, starost15_64, by.x = "NAME", by.y = "country")) + 
 tm_polygons("percentage", midpoint = 0.2)
-
-print(zemljevid15_64)
