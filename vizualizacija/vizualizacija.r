8d5bf64fffee145d353d82f394ca3df4089811c0
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2020-02-25 22:29:03 +0100

    Začetek dela na religijah

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -13,27 +13,35 @@
 #   geom_histogram(binwidth = 0.7) + facet_wrap(~year)
 #print(starostneStruture65_graf)
 
-
 procenti_poLetih <- vsote %>% mutate(procent_014 = 100 * prva/total, procent_1564 = 100 * druga/total,
                                      procent_65 = 100 * tretja/total) %>%
   select(-"total", -"prva", -"druga", -"tretja") %>% 
-  gather(group, procent, procent_014, procent_1564, procent_65, -year) %>% arrange(year) %>%
+  gather(group, procent, "procent_014":"procent_65", -year) %>% arrange(year) %>%
   mutate(percentage = procent,Age_group = parse_number(group) %>% 
-           factor(levels=c(14,1564,65), labels = c("0-14","15-64","65+"), ordered=TRUE))
+           factor(levels=c(14,1564,65), labels = c("0-14","15-64","65+"), ordered=TRUE)) %>% select(-procent, -group)
+procenti_poLetih <- procenti_poLetih[c(1,3,2)]
+View(procenti_poLetih)
+
 
 procenti_poLetih_graf <- ggplot(procenti_poLetih, aes(x=year,y=percentage, color=Age_group)) + 
   geom_col(position=position_dodge2(preserve = "total"), fill = 'white') 
 
+
+
+
+
 #print(procenti_poLetih_graf)
 
 
-#BDP ppp IN STAROSTNE STRUKTURE GRAFI
+#BDP(ppp) IN STAROSTNE STRUKTURE GRAFI
 
-bdp_starostneStrukture014 <- inner_join(bdpji, StarostneStrukture_janos, by = c("year", "country")) %>% 
+bdp_starostneStrukture014 <- inner_join(bdpji_ppp, StarostneStrukture_janos, by = c("year", "country")) %>% 
   filter(Age_group == "0-14")
 bdp_starostne014_graf <- ggplot(bdp_starostneStrukture014, aes(x=gdp, y=percentage)) +
   geom_point() + scale_x_log10() + facet_wrap(~year)
-#print(bdp_starostne014_graf)
+
+
+print(bdp_starostne014_graf)
 
 # + geom_hline(yintercept = group_by(bdp_starostneStrukture65, year) %>%
 #               summarise(povp = mean(percentage)))
@@ -45,12 +53,21 @@ bdp_starostne65_graf <- ggplot(bdp_starostneStrukture65, aes(x=gdp, y=percentage
   geom_point() + scale_x_log10() + facet_wrap(~year)
 
 
-#print(bdp_starostne65_graf)
-
 
 #RELIGIJE IN STAROSTNE STRUKTURE
-#nimam pojma kaj zaenkrat
 
+krscanske <- religion_tidy %>% filter(religion == "christians") %>% filter(percentage >= 70, percentage <= 100)
+krscanske_starostneStrukture <- inner_join(StarostneStruktureProcent, krscanske, by = "country") %>% 
+   mutate(percentage = percentage.x) %>% select(-"percentage.y", -"religion", -"percentage.x")
+
+krscanske_starostneStrukture_graf <- ggplot(krscanske_starostneStrukture, aes(x=percentage)) +
+  geom_histogram() + facet_grid(year~Age_group)
+print(krscanske_starostneStrukture_graf)
+
+
+
+muslimanske <- religion_tidy %>% filter(religion == "muslims") %>% filter(percentage >= 70, percentage <= 100) 
+budisticne <- religion_tidy %>% filter(religion == "buddhists") %>% filter(percentage >= 70) 
 
 
 
