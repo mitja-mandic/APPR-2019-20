6ff6e45f79d96c391f428f352b2c79beceb69de2
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2020-03-01 19:05:04 +0100

    Dodana populacija in hdi

--- shiny/app.R
+++ shiny/app.R
@@ -1,30 +1,46 @@
 ui <- fluidPage(
-  titlePanel("Igranje"),
+  titlePanel("Graf starostnih struktur in HDI/BDP/populacije"),
   sidebarLayout(
     position = "right",
-    sidebarPanel("kaj na graf",
+    sidebarPanel("Parametri za graf",
+                selectInput(inputId = "leto",
+                            label = "Leto",
+                            choices = c("skupaj", leta2),
+                            selected = "skupaj"),
                 selectInput(inputId = "podatek", 
                             label = "Parametri za graf",
                             choices = c("BDP" = "gdp",
-                                        "HDI" = "HDI"),
+                                        "HDI" = "HDI",
+                                        "Populacija" = "population"),
                             br()),
                 radioButtons(inputId = "skupina",
                              label = "Starostna skupina",
                              choices = c("0-14",
                                          "15-64",
-                                         "65+"))
+                                         "65+")),
   ),
-  mainPanel(plotOutput("graf"))
+  mainPanel(plotOutput("graf", height = "400px", width = "700px"))
   )
 )
 
 
+
 server <- function(input, output) {
   y <- reactive({
-  za_graf <- bdp_starostneStrutkure %>% filter(Age_group == input$skupina)
+    if (input$leto != "skupaj"){
+    za_graf <- bdp_starostneStrutkure %>% filter(Age_group == input$skupina, year == input$leto)
     ggplot(za_graf,
            aes(x = za_graf[ ,input$podatek], y = percentage)) + geom_point() + 
-      scale_x_log10()
+      scale_x_log10() + scale_y_log10() +  xlab(input$podatek)
+    
+    }
+    else{
+      za_graf <- bdp_starostneStrutkure %>% filter(Age_group == input$skupina)
+      ggplot(za_graf,
+             aes(x = za_graf[ ,input$podatek], y = percentage, color = factor(year))) + geom_point() + 
+        scale_x_log10() + scale_y_log10() + xlab(input$podatek)
+      
+    }
   })
   output$graf <- renderPlot(y())
 }
