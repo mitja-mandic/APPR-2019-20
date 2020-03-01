5bc47cbada9b12d79b356a87c3e2d9a7d7399bd9
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2020-03-01 10:36:11 +0100

    Začetek dela na Shinyju, lepi rezultati

--- shiny/app.R
+++ shiny/app.R
@@ -1,9 +1,37 @@
-ui <- fluidPage()
+ui <- fluidPage(
+  titlePanel("Igranje"),
+  sidebarLayout(
+    position = "right",
+    sidebarPanel("kaj na graf",
+                selectInput(inputId = "podatek", 
+                            label = "Parametri za graf",
+                            choices = c("BDP" = "gdp",
+                                        "HDI" = "HDI"),
+                            br()),
+                radioButtons(inputId = "skupina",
+                             label = "Starostna skupina",
+                             choices = c("0-14",
+                                         "15-64",
+                                         "65+"))
+  ),
+  mainPanel(plotOutput("graf"))
+  )
+)
+
+
+server <- function(input, output) {
+  y <- reactive({
+  za_graf <- bdp_starostneStrutkure %>% filter(Age_group == input$skupina)
+    ggplot(za_graf,
+           aes(x = za_graf[ ,input$podatek], y = percentage)) + geom_point() + 
+      scale_x_log10()
+  })
+  output$graf <- renderPlot(y())
+}
 
 
 
 
 
-server <- function(input, output) {}
 
 shinyApp(ui = ui, server = server)
