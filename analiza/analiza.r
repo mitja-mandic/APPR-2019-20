2900037ff862b00f741ae1a338148e697bcb0817
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2020-03-06 19:25:52 +0100

    začetek dela na napredni analizi

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,14 +1,11 @@
-# # 4. faza: Analiza podatkov
-# 
-# podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-#                                 gostota.naselij=naselja/povrsina) %>%
-#   left_join(povprecja, by="obcina")
-# row.names(podatki) <- podatki$obcina
-# podatki$obcina <- NULL
-# 
-# # Število skupin
-# n <- 5
-# skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
-# 
-# 
+poskus <- bdp_starostneStrutkure %>% filter(Age_group == "0-14") %>% select(-gdp_ppp,-HDI, population) %>%
+  na.omit()
 
+model <- lm(bdp_pc ~ percentage, data = poskus)
+
+bdp_ss_predict <- predict(model, interval = "confidence")
+podatki <- cbind(poskus, bdp_ss_predict)
+
+bdp_starostne_graf <- ggplot(podatki, aes(x = bdp_pc, y = percentage)) + facet_wrap(year~.) + geom_point() + 
+  geom_smooth(method = loess,  fullrange = FALSE, col = "blue")  + scale_x_log10()
+print(bdp_starostne_graf)
