91d411ed1fbeda05706b2377ee3a23fe031fc738
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2020-03-07 22:02:32 +0100

    Dodan tab na shiny za izobrazbo

--- shiny/app_bdp.R
+++ shiny/app_bdp.R
@@ -1,38 +1,62 @@
 ui <- fluidPage(
   titlePanel("Graf starostnih struktur in HDI/BDP/populacije"),
-  sidebarLayout(
-    position = "left",
-    sidebarPanel("Parametri za graf",
-                selectInput(inputId = "leto",
-                            label = "Leto",
-                            choices = c("skupaj", leta2),
-                            selected = "skupaj"),
-                selectInput(inputId = "podatek", 
-                            label = "Parametri za graf",
-                            choices = c("BDP PPP" = "gdp_ppp",
-                                        "BDP pc" = "bdp_pc",
-                                        "HDI" = "HDI",
-                                        "Populacija" = "population")),
-                radioButtons(inputId = "skupina",
-                             label = "Starostna skupina",
-                             choices = c("0-14",
-                                         "15-64",
-                                         "65+")),
-                selectInput(inputId = "smooth",
-                            label = "Prileganje",
-                            choices = c("brez",
-                                        "Linearno prileganje" = "lm",
-                                        "GLM" = "glm",
-                                        "LOESS" = "loess"), selected = "brez")),
-  mainPanel(plotOutput("graf", height = "400px", width = "600px"))
+  tabsetPanel(
+    tabPanel("BDP",
+      sidebarLayout(
+        position = "left",
+        sidebarPanel("Parametri za graf",
+                    selectInput(inputId = "leto1",
+                                label = "Leto",
+                                choices = c("skupaj", leta2),
+                                selected = "skupaj"),
+                    selectInput(inputId = "podatek", 
+                                label = "Parametri za graf",
+                                choices = c("BDP PPP" = "gdp_ppp",
+                                            "BDP pc" = "bdp_pc",
+                                            "HDI" = "HDI",
+                                            "Populacija" = "population")),
+                    radioButtons(inputId = "skupina",
+                                 label = "Starostna skupina",
+                                 choices = c("0-14",
+                                             "15-64",
+                                             "65+")),
+                    selectInput(inputId = "smooth",
+                                label = "Prileganje",
+                                choices = c("brez",
+                                            "Linearno prileganje" = "lm",
+                                            "GLM" = "glm",
+                                            "LOESS" = "loess"), selected = "brez")),
+    mainPanel(plotOutput("grafBDP", height = "400px", width = "600px")),
+  )
+),
+tabPanel("Izobrazba",
+         sidebarLayout(
+           position = "left",
+           sidebarPanel("Parametri za graf",
+                        selectInput(inputId = "leto2",
+                                    label = "Leto",
+                                    choices = c("skupaj", leta2),
+                                    selected = "skupaj"),
+                        selectInput(inputId = "izo",
+                                    label = "Stopnja izobrazbe",
+                                    choices = c("Primarna" = "primary",
+                                                "Sekundarna" = "secondary",
+                                                "Terciarna" = "tertiary")),
+                        radioButtons(inputId = "skupina2",
+                                     label = "Starostna skupina",
+                                     choices = c("0-14",
+                                                 "15-64",
+                                                 "65+"))),
+                        mainPanel(plotOutput("grafIZO")),
+      )
+    )
   )
 )
 
-
 server <- function(input, output) {
   y <- reactive({
-    if (input$leto != "skupaj"){
-    za_graf <- bdp_starostneStrutkure %>% filter(Age_group == input$skupina, year == input$leto)
+    if (input$leto1 != "skupaj"){
+    za_graf <- bdp_starostneStrutkure %>% filter(Age_group == input$skupina, year == input$leto1)
     ggplot(za_graf,
            aes(x = za_graf[ ,input$podatek], y = percentage)) + geom_point() + 
       scale_x_log10() + scale_y_log10() +  xlab(input$podatek) + geom_smooth(method = input$smooth, fullrange = FALSE)
@@ -46,9 +70,19 @@ server <- function(input, output) {
       
     }
   })
-  output$graf <- renderPlot(y())
+  z <- reactive({
+    if(input$leto2 != "skupaj"){
+      grafIzo <- izo_starostne %>% filter(Age_group == input$skupina2, year == input$leto2, series == input$izo)
+      ggplot(grafIzo, aes(x = percentageIzo, y = percentage, color)) + geom_point()
+    }
+    else{
+      grafIzo <- izo_starostne %>% filter(Age_group == input$skupina2, series == input$izo)
+      ggplot(grafIzo, aes(x = percentageIzo, y = percentage)) + geom_point()
+    }
+  })
+  output$grafBDP <- renderPlot(y())
+  output$grafIZO <- renderPlot(z())
 }
-
 
 
 
