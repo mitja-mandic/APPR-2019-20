190db048949f4e50d327f23472b1b34af1015fee
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2020-03-09 19:04:30 +0100

    Preimenovan, dodana možnost izbire skale na oseh

--- shiny/app.R
+++ shiny/app.R
@@ -25,10 +25,15 @@ ui <- fluidPage(
                                 choices = c("brez",
                                             "Linearno prileganje" = "lm",
                                             "GLM" = "glm",
-                                            "LOESS" = "loess"), selected = "brez")),
-    mainPanel(plotOutput("grafBDP", height = "400px", width = "600px")),
-  )
-),
+                                            "LOESS" = "loess"), selected = "brez"),
+                    checkboxGroupInput(inputId = "log1",
+                                       label  = "Skala",
+                                       choices = c("x logaritmirana" = "logx",
+                                                   "y logaritmirana" = "logy"),
+                                                   selected = c("logx","logy"))),
+    mainPanel(plotOutput("grafBDP", height = "400px", width = "600px"))
+    )
+  ),
 tabPanel("Izobrazba",
          sidebarLayout(
            position = "left",
@@ -52,30 +57,78 @@ tabPanel("Izobrazba",
                                     choices = c("brez",
                                                 "Linearno prileganje" = "lm",
                                                 "GLM" = "glm",
-                                                "LOESS" = "loess"), selected = "brez")),
-                        mainPanel(plotOutput("grafIZO")),
+                                                "LOESS" = "loess"), selected = "brez"),
+                        checkboxGroupInput(inputId = "log2",
+                                           label  = "Skala",
+                                           choices = c("x logaritmirana" = "logx",
+                                                       "y logaritmirana" = "logy"),
+                                           selected = c("logx","logy"))),
+                        mainPanel(plotOutput("grafIZO"))
       )
     )
   )
 )
 
 server <- function(input, output) {
+  x <- reactive({input$log1})
   y <- reactive({
+    if (length(x()) == 2){
+      if (input$leto1 != "skupaj"){
+      za_graf <- bdp_starostneStrutkure %>% filter(Age_group == input$skupina, year == input$leto1)
+      ggplot(za_graf,
+             aes(x = za_graf[ ,input$podatek], y = percentage)) + geom_point() + 
+        scale_x_log10() + scale_y_log10() +  xlab(input$podatek) + geom_smooth(method = input$smooth, fullrange = FALSE)
+      }
+      else{
+        za_graf <- bdp_starostneStrutkure %>% filter(Age_group == input$skupina)
+        ggplot(za_graf,
+               aes(x = za_graf[ ,input$podatek], y = percentage, color = factor(year))) + geom_point() + 
+          scale_x_log10() + scale_y_log10() + xlab(input$podatek) + geom_smooth(method = input$smooth, se = FALSE, fullrange = FALSE)
+      }
+    }
+    else if(length(x()) == 1){
+      if(x() == "logx"){
+        if (input$leto1 != "skupaj"){
+          za_graf <- bdp_starostneStrutkure %>% filter(Age_group == input$skupina, year == input$leto1)
+          ggplot(za_graf,aes(x = za_graf[ ,input$podatek], y = percentage)) + geom_point() + 
+            scale_x_log10() +  xlab(input$podatek) + geom_smooth(method = input$smooth, fullrange = FALSE)
+        }
+        else{
+          za_graf <- bdp_starostneStrutkure %>% filter(Age_group == input$skupina)
+          ggplot(za_graf,aes(x = za_graf[ ,input$podatek], y = percentage, color = factor(year))) + geom_point() + 
+            scale_x_log10() + xlab(input$podatek) + geom_smooth(method = input$smooth, se = FALSE, fullrange = FALSE)
+        }
+      }
+      else{
+        if (input$leto1 != "skupaj"){
+          za_graf <- bdp_starostneStrutkure %>% filter(Age_group == input$skupina, year == input$leto1)
+          ggplot(za_graf, aes(x = za_graf[ ,input$podatek], y = percentage)) + geom_point() + scale_y_log10() +  
+            xlab(input$podatek) + geom_smooth(method = input$smooth, fullrange = FALSE)
+        }
+        else{
+          za_graf <- bdp_starostneStrutkure %>% filter(Age_group == input$skupina)
+          ggplot(za_graf, aes(x = za_graf[ ,input$podatek], y = percentage, color = factor(year))) + geom_point() + scale_y_log10() +
+            xlab(input$podatek) + geom_smooth(method = input$smooth, fullrange = FALSE)
+          
+        }
+      }
+    }
+  else{
     if (input$leto1 != "skupaj"){
-    za_graf <- bdp_starostneStrutkure %>% filter(Age_group == input$skupina, year == input$leto1)
-    ggplot(za_graf,
-           aes(x = za_graf[ ,input$podatek], y = percentage)) + geom_point() + 
-      scale_x_log10() + scale_y_log10() +  xlab(input$podatek) + geom_smooth(method = input$smooth, fullrange = FALSE)
-    
+      za_graf <- bdp_starostneStrutkure %>% filter(Age_group == input$skupina, year == input$leto1)
+      ggplot(za_graf,
+             aes(x = za_graf[ ,input$podatek], y = percentage)) + geom_point() + 
+        xlab(input$podatek) + geom_smooth(method = input$smooth, fullrange = FALSE)
     }
     else{
       za_graf <- bdp_starostneStrutkure %>% filter(Age_group == input$skupina)
       ggplot(za_graf,
              aes(x = za_graf[ ,input$podatek], y = percentage, color = factor(year))) + geom_point() + 
-        scale_x_log10() + scale_y_log10() + xlab(input$podatek) + geom_smooth(method = input$smooth, se = FALSE, fullrange = FALSE)
-      
+         xlab(input$podatek) + geom_smooth(method = input$smooth, se = FALSE, fullrange = FALSE)
     }
-  })
+  }
+})
+  
   z <- reactive({
     if(input$leto2 != "skupaj"){
       grafIzo <- izo_starostne %>% filter(Age_group == input$skupina2, year == input$leto2, series == input$izo)
