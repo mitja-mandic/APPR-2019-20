256d4ecc4787436e646c64d3c7250086c5b0cf83
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2020-03-10 22:19:40 +0100

    loess naprej

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -9,5 +9,10 @@ napoved_014$percentageLm <- predict(model014_lm, napoved_014)
 
 
 model014_loess <- loess(data = procent_014, percentage~year)
-
 napoved_014$percentageLoess <- predict(model014_loess, napoved_014)
+
+
+
+
+graf_014 <- ggplot(procent_014, aes(x = year, y = percentage)) + geom_line() + 
+  geom_line(data = napoved_014, aes(y = percentageLm, color = "red")) + geom_line(data = napoved_014, aes(y = percentageLoess, color = "blue"))
