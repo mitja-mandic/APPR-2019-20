521192b6d9e064eba71b1b12dd17c33292d15780
Mitja Mandić <49949550+mitja-mandic@users.noreply.github.com>
2020-03-10 20:53:18 +0100

    Dodana opcija izbire skale na grafu izobrazbe

--- shiny/app.R
+++ shiny/app.R
@@ -60,9 +60,9 @@ tabPanel("Izobrazba",
                                                 "LOESS" = "loess"), selected = "brez"),
                         checkboxGroupInput(inputId = "log2",
                                            label  = "Skala",
-                                           choices = c("x logaritmirana" = "logx",
-                                                       "y logaritmirana" = "logy"),
-                                           selected = c("logx","logy"))),
+                                           choices = c("x logaritmirana" = "logx2",
+                                                       "y logaritmirana" = "logy2"),
+                                           selected = c("logx2","logy2"))),
                         mainPanel(plotOutput("grafIZO"))
       )
     )
@@ -103,7 +103,7 @@ server <- function(input, output) {
         if (input$leto1 != "skupaj"){
           za_graf <- bdp_starostneStrutkure %>% filter(Age_group == input$skupina, year == input$leto1)
           ggplot(za_graf, aes(x = za_graf[ ,input$podatek], y = percentage)) + geom_point() + scale_y_log10() +  
-            xlab(input$podatek) + geom_smooth(method = input$smooth, fullrange = FALSE)
+            xlab(input$podatek) + geom_smooth(method = input$smooth, se = FALSE, fullrange = FALSE)
         }
         else{
           za_graf <- bdp_starostneStrutkure %>% filter(Age_group == input$skupina)
@@ -128,16 +128,57 @@ server <- function(input, output) {
     }
   }
 })
-  
+  w <- reactive({input$log2})
   z <- reactive({
-    if(input$leto2 != "skupaj"){
-      grafIzo <- izo_starostne %>% filter(Age_group == input$skupina2, year == input$leto2, series == input$izo)
-      ggplot(grafIzo, aes(x = percentageIzo, y = percentage)) + geom_point() + geom_smooth(method = input$smooth2, fullrange = FALSE)
+    if(length(w()) == 2){  
+      if(input$leto2 != "skupaj"){
+        grafIzo <- izo_starostne %>% filter(Age_group == input$skupina2, year == input$leto2, series == input$izo)
+        ggplot(grafIzo, aes(x = percentageIzo, y = percentage)) + geom_point() + geom_smooth(method = input$smooth2, fullrange = FALSE) + 
+        scale_x_log10() + scale_y_log10()                                                                                      
+      }
+      else{
+        grafIzo <- izo_starostne %>% filter(Age_group == input$skupina2, series == input$izo)
+        ggplot(grafIzo, aes(x = percentageIzo, y = percentage, color = factor(year))) + geom_point() + 
+        geom_smooth(method = input$smooth2, fullrange = FALSE, se = FALSE) + 
+        scale_x_log10() + scale_y_log10()
+      }
     }
-    else{
-      grafIzo <- izo_starostne %>% filter(Age_group == input$skupina2, series == input$izo)
-      ggplot(grafIzo, aes(x = percentageIzo, y = percentage, color = factor(year))) + geom_point() + 
-      geom_smooth(method = input$smooth2, fullrange = FALSE, se = FALSE)
+    else if(length(w()) == 1){
+      if(input$log2 == "logx2"){
+        if(input$leto2 != "skupaj"){
+          grafIzo <- izo_starostne %>% filter(Age_group == input$skupina2, year == input$leto2, series == input$izo)
+          ggplot(grafIzo, aes(x = percentageIzo, y = percentage)) + geom_point() + 
+          geom_smooth(method = input$smooth2, fullrange = FALSE) + scale_x_log10()
+        }
+        else{
+          grafIzo <- izo_starostne %>% filter(Age_group == input$skupina2, series == input$izo)
+          ggplot(grafIzo, aes(x = percentageIzo, y = percentage, color = factor(year))) + geom_point() + 
+            geom_smooth(method = input$smooth2, fullrange = FALSE, se = FALSE) + scale_x_log10()
+        }
+      }
+      else{
+        if(input$leto2 != "skupaj"){
+          grafIzo <- izo_starostne %>% filter(Age_group == input$skupina2, year == input$leto2, series == input$izo)
+          ggplot(grafIzo, aes(x = percentageIzo, y = percentage)) + geom_point() + 
+            geom_smooth(method = input$smooth2, fullrange = FALSE) + scale_y_log10()
+        }
+        else{
+          grafIzo <- izo_starostne %>% filter(Age_group == input$skupina2, series == input$izo)
+          ggplot(grafIzo, aes(x = percentageIzo, y = percentage, color = factor(year))) + geom_point() + 
+            geom_smooth(method = input$smooth2, fullrange = FALSE, se = FALSE) + scale_y_log10()
+        }
+      }
+    }
+    else if(length(w()) == 0){
+      if(input$leto2 != "skupaj"){
+        grafIzo <- izo_starostne %>% filter(Age_group == input$skupina2, year == input$leto2, series == input$izo)
+        ggplot(grafIzo, aes(x = percentageIzo, y = percentage)) + geom_point() + geom_smooth(method = input$smooth2, fullrange = FALSE)
+      }
+      else{
+        grafIzo <- izo_starostne %>% filter(Age_group == input$skupina2, series == input$izo)
+        ggplot(grafIzo, aes(x = percentageIzo, y = percentage, color = factor(year))) + geom_point() + 
+          geom_smooth(method = input$smooth2, fullrange = FALSE, se = FALSE)
+      }
     }
   })
   output$grafBDP <- renderPlot(y())
